e073ece2bc82818c790b39f2074cf25b6a6e0043
Ultimate-Pamoi <127316136+HAMAMO000@users.noreply.github.com>
2024-12-19 02:52:26 +0900

    Update index.js

--- homepage/javascript/index.js
+++ homepage/javascript/index.js
@@ -1,1 +1,24 @@
+const animationDuration = 700; // アニメーション時間 (ms)
 
+function accessto(url) {
+    const cover1 = document.getElementById("cover1");
+    const cover2 = document.getElementById("cover2");
+    // ページを覆うアニメーション
+    cover1.style.transform = "translateY(0%)";
+    cover2.style.transform = "translateY(0%)";
+
+    // アニメーションが完了した後にページ遷移
+    setTimeout(() => {
+        window.location.href = url;
+    }, animationDuration);
+}
+
+window.addEventListener("load", () => {
+    const cover1 = document.getElementById("cover1");
+    const cover2 = document.getElementById("cover2");
+    // ページ読み込み時にシャッターを開くアニメーション
+    setTimeout(() => {
+        cover1.style.transform = "translateY(-100%)";
+        cover2.style.transform = "translateY(100%)";
+    }, 100);
+});
